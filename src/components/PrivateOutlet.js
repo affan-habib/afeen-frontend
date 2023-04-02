a1b3863f8f7a480d6ce610ac40e29230411c570e
affan-habib1 <affan.gononet@gmail.com>
2023-04-02 17:52:24 +0600

    removed components

--- src/components/PrivateOutlet.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
-
-export default function PrivateOutlet() {
-  const auth = useAuth();
-
-  return auth ? <Outlet /> : <Navigate to="/login" />;
-}
