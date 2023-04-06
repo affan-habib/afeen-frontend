6d543ee621c9b7c6a533c305af78849a75474edb
affan-habib <affan.habib44@gmail.com>
2023-04-06 17:47:58 +0600

    filter addd

--- src/pages/job/FilterJobs.js
+++ src/pages/job/FilterJobs.js
@@ -6,19 +6,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { callApi, selectApi } from "store/reducers/apiSlice";
 
 const FilterJobs = () => {
-  const [category, setCategoryId] = useState('');
+  const [category, setCategoryId] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [minSalary, setMinSalary] = useState("");
+  const [maxSalary, setMaxSalary] = useState("");
   const dispatch = useDispatch();
   const { jobCategory } = useSelector(selectApi);
   React.useEffect(() => {
+    fetchData();
+  }, [category]);
+
+  function fetchData() {
     dispatch(
       callApi({
         operationId: UrlBuilder.jobServiceApi(
-          `jobs/all-post?limit=10&page=1&job_category_ids=${category}`
+          `jobs/all-post?limit=10&page=1&job_category_ids=${category}&salary_min=${minSalary}&salary_max=${maxSalary}&keyword=${keyword}`
         ),
         output: "users",
       })
     );
-  }, [category]);
+  }
+
   React.useEffect(() => {
     dispatch(
       callApi({
@@ -29,19 +37,32 @@ const FilterJobs = () => {
   }, []);
   return (
     <Stack mb={2} spacing={2} direction="row">
-      <TextField label="SEARCH BY TITLE" />
-      <TextField label="MIN SALARY" />
-      <TextField label="MAX SALARY" />
+      <TextField
+        onChange={(e) => setKeyword(e.target.value)}
+        label="SEARCH BY TITLE"
+        sx={{ width: 150 }}
+      />
+      <TextField
+        label="MIN SALARY"
+        onChange={(e) => setMinSalary(e.target.value)}
+        sx={{ width: 150 }}
+      />
+
+      <TextField
+        label="MAX SALARY"
+        onChange={(e) => setMaxSalary(e.target.value)}
+        sx={{ width: 150 }}
+      />
       <Select
         label="Category"
-        sx={{ width: 100 }}
+        sx={{ width: 150 }}
         onChange={(e) => setCategoryId(e.target.value)}
       >
         {jobCategory?.data.map((category) => (
           <MenuItem value={category.id}>{category.name}</MenuItem>
         ))}
       </Select>
-      <Button startIcon={<Search />} variant="contained">
+      <Button startIcon={<Search />} variant="contained" onClick={fetchData}>
         Search
       </Button>
     </Stack>
