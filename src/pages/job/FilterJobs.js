a7d41d6dc0c3695bb14b34bede68a306ad21b7fb
affan-habib <affan.habib44@gmail.com>
2023-04-06 16:59:01 +0600

    filter addd

--- src/pages/job/FilterJobs.js
+++ src/pages/job/FilterJobs.js
@@ -1,17 +1,29 @@
 import { Search } from "@mui/icons-material";
 import { Box, Button, MenuItem, Select, Stack, TextField } from "@mui/material";
 import { UrlBuilder } from "helpers/UrlBuilder";
-import React from "react";
-import { useDispatch } from "react-redux";
-import { callApi } from "store/reducers/apiSlice";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { callApi, selectApi } from "store/reducers/apiSlice";
 
 const FilterJobs = () => {
+  const [category, setCategoryId] = useState(null);
   const dispatch = useDispatch();
+  const { jobCategory } = useSelector(selectApi);
   React.useEffect(() => {
     dispatch(
       callApi({
-        operationId: UrlBuilder.jobServiceApi("jobs/all-post?limit=10&page=1"),
+        operationId: UrlBuilder.jobServiceApi(
+          `jobs/all-post?limit=10&page=1&job_category_ids=${category}`
+        ),
         output: "users",
+      })
+    );
+  }, [category]);
+  React.useEffect(() => {
+    dispatch(
+      callApi({
+        operationId: UrlBuilder.jobServiceApi("job/categories"),
+        output: "jobCategory",
       })
     );
   }, []);
@@ -20,12 +32,18 @@ const FilterJobs = () => {
       <TextField label="SEARCH BY TITLE" />
       <TextField label="MIN SALARY" />
       <TextField label="MAX SALARY" />
-      <Select value={1} sx={{ width: 100 }}>
-        <MenuItem value="1">Dhaka</MenuItem>
-        <MenuItem value="2">Chittagong</MenuItem>
-        <MenuItem value="3">Sylhet</MenuItem>
+      <Select
+        label="Category"
+        sx={{ width: 100 }}
+        onChange={(e) => setCategoryId(e.target.value)}
+      >
+        {jobCategory?.data.map((category) => (
+          <MenuItem value={category.id}>{category.name}</MenuItem>
+        ))}
       </Select>
-      <Button startIcon={<Search/>} variant="contained">Search</Button>
+      <Button startIcon={<Search />} variant="contained">
+        Search
+      </Button>
     </Stack>
   );
 };
