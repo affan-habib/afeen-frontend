eb4aca34d6beb0505f36ec8475f77c5cc4ae593f
affan-habib <affan.habib44@gmail.com>
2023-04-16 13:29:06 +0600

    breadcumb fixed

--- src/components/@extended/Breadcrumbs.js
+++ src/components/@extended/Breadcrumbs.js
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 // material-ui
-import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
-import { useTheme } from '@mui/material/styles';
-import { Divider, Grid, Typography } from '@mui/material';
+import MuiBreadcrumbs from "@mui/material/Breadcrumbs";
+import { useTheme } from "@mui/material/styles";
+import { Divider, Grid, Typography } from "@mui/material";
 
 // project imports
-import MainCard from '../MainCard';
+import MainCard from "../MainCard";
 
 // assets
-import { ApartmentOutlined, HomeFilled, HomeOutlined } from '@ant-design/icons';
+import { ApartmentOutlined, HomeFilled, HomeOutlined } from "@ant-design/icons";
 
 // ==============================|| BREADCRUMBS ||============================== //
 
@@ -37,18 +37,18 @@ const Breadcrumbs = ({
   const iconSX = {
     marginRight: theme.spacing(0.75),
     marginTop: `-${theme.spacing(0.25)}`,
-    width: '1rem',
-    height: '1rem',
-    color: theme.palette.secondary.main
+    width: "1rem",
+    height: "1rem",
+    color: theme.palette.secondary.main,
   };
 
   // set active item state
   const getCollapse = (menu) => {
     if (menu.children) {
       menu.children.filter((collapse) => {
-        if (collapse.type && collapse.type === 'collapse') {
+        if (collapse.type && collapse.type === "collapse") {
           getCollapse(collapse);
-        } else if (collapse.type && collapse.type === 'item') {
+        } else if (collapse.type && collapse.type === "item") {
           if (location.pathname === collapse.url) {
             setMain(menu);
             setItem(collapse);
@@ -61,7 +61,7 @@ const Breadcrumbs = ({
 
   useEffect(() => {
     navigation?.items?.map((menu) => {
-      if (menu.type && menu.type === 'group') {
+      if (menu.type && menu.type === "group") {
         getCollapse(menu);
       }
       return false;
@@ -69,26 +69,36 @@ const Breadcrumbs = ({
   });
 
   // only used for component demo breadcrumbs
-  if (location.pathname === '/components-overview/breadcrumbs') {
-    location.pathname = '/dashboard/analytics';
+  if (location.pathname === "/components-overview/breadcrumbs") {
+    location.pathname = "/dashboard/analytics";
   }
 
   // item separator
   const SeparatorIcon = separator;
-  const separatorIcon = separator ? <SeparatorIcon style={{ fontSize: '0.75rem', marginTop: 2 }} /> : '/';
+  const separatorIcon = separator ? (
+    <SeparatorIcon style={{ fontSize: "0.75rem", marginTop: 2 }} />
+  ) : (
+    "/"
+  );
 
   let mainContent;
   let itemContent;
   let breadcrumbContent = <Typography />;
-  let itemTitle = '';
+  let itemTitle = "";
   let CollapseIcon;
   let ItemIcon;
 
   // collapse item
-  if (main && main.type === 'collapse') {
+  if (main && main.type === "collapse") {
     CollapseIcon = main.icon ? main.icon : ApartmentOutlined;
     mainContent = (
-      <Typography component={Link} to={document.location.pathname} variant="h6" sx={{ textDecoration: 'none' }} color="textSecondary">
+      <Typography
+        component={Link}
+        to={document.location.pathname}
+        variant="h6"
+        sx={{ textDecoration: "none" }}
+        color="textSecondary"
+      >
         {icons && <CollapseIcon style={iconSX} />}
         {main.title}
       </Typography>
@@ -96,7 +106,7 @@ const Breadcrumbs = ({
   }
 
   // items
-  if (item && item.type === 'item') {
+  if (item && item.type === "item") {
     itemTitle = item.title;
 
     ItemIcon = item.icon ? item.icon : ApartmentOutlined;
@@ -112,16 +122,20 @@ const Breadcrumbs = ({
       breadcrumbContent = (
         <MainCard
           border={card}
-          sx={card === false ? { mb: 3, bgcolor: 'transparent', ...sx } : { mb: 3, ...sx }}
+          sx={
+            card === false
+              ? { mb: 3, bgcolor: "transparent", ...sx }
+              : { mb: 3, ...sx }
+          }
           {...others}
           content={card}
           shadow="none"
         >
           <Grid
             container
-            direction={rightAlign ? 'row' : 'column'}
-            justifyContent={rightAlign ? 'space-between' : 'flex-start'}
-            alignItems={rightAlign ? 'center' : 'flex-start'}
+            direction={rightAlign ? "row" : "column"}
+            justifyContent={rightAlign ? "space-between" : "flex-start"}
+            alignItems={rightAlign ? "center" : "flex-start"}
             spacing={1}
           >
             {title && !titleBottom && (
@@ -130,21 +144,28 @@ const Breadcrumbs = ({
               </Grid>
             )}
             <Grid item>
-              <MuiBreadcrumbs aria-label="breadcrumb" maxItems={maxItems || 8} separator={separatorIcon}>
-                <Typography component={Link} to="/" color="textSecondary" variant="h6" sx={{ textDecoration: 'none' }}>
+              <MuiBreadcrumbs
+                aria-label="breadcrumb"
+                maxItems={maxItems || 8}
+                separator={separatorIcon}
+              >
+                <Typography
+                  component={Link}
+                  to="/"
+                  color="textSecondary"
+                  variant="h6"
+                  sx={{ textDecoration: "none" }}
+                >
                   {icons && <HomeOutlined style={iconSX} />}
-                  {icon && !icons && <HomeFilled style={{ ...iconSX, marginRight: 0 }} />}
-                  {(!icon || icons) && 'Home'}
+                  {icon && !icons && (
+                    <HomeFilled style={{ ...iconSX, marginRight: 0 }} />
+                  )}
+                  {(!icon || icons) && "Home"}
                 </Typography>
                 {mainContent}
                 {itemContent}
               </MuiBreadcrumbs>
             </Grid>
-            {title && titleBottom && (
-              <Grid item sx={{ mt: card === false ? 0.25 : 1 }}>
-                <Typography variant="h2">{item.title}</Typography>
-              </Grid>
-            )}
           </Grid>
           {card === false && divider !== false && <Divider sx={{ mt: 2 }} />}
         </MainCard>
@@ -166,7 +187,7 @@ Breadcrumbs.propTypes = {
   separator: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
   title: PropTypes.bool,
   titleBottom: PropTypes.bool,
-  sx: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+  sx: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 };
 
 export default Breadcrumbs;
