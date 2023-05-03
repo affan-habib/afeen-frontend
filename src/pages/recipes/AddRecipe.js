2e517080f113775adda68c6f5dee14c2e95a416d
Affan <affan.habib44@gmail.com>
2023-05-03 23:23:05 +0600

    grid added

--- src/pages/recipes/AddRecipe.js
+++ src/pages/recipes/AddRecipe.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { Formik, Form, Field, FieldArray } from "formik";
 import * as Yup from "yup";
-import { Button, Box, Typography, IconButton } from "@mui/material";
+import { Button, Grid, IconButton } from "@mui/material";
 import { DeleteColumnOutlined } from "@ant-design/icons";
 import CustomTextField from "components/CustomTextField";
+import CustomCard from "components/CustomCard";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required("Required"),
@@ -23,153 +24,151 @@ const initialValues = {
 
 export default function AddRecipe() {
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ values, errors, touched }) => (
-        <Form>
-          <>
-            <Box mt={2}>
-              <Typography variant="h5" component="h2">
-                Recipe Form
-              </Typography>
-            </Box>
-            <Box mt={2}>
-              <Field
-                name="title"
-                as={CustomTextField}
-                inputLabel="Title"
-                fullWidth
-                error={errors.title && touched.title}
-                helperText={errors.title && touched.title && errors.title}
-              />
-            </Box>
-            <Box mt={2}>
-              <Field
-                name="description"
-                as={CustomTextField}
-                inputLabel="Description"
-                fullWidth
-                multiline
-                rows={4}
-                error={errors.description && touched.description}
-                helperText={
-                  errors.description &&
-                  touched.description &&
-                  errors.description
-                }
-              />
-            </Box>
-            <Box mt={2}>
-              <Field
-                name="category"
-                as={CustomTextField}
-                inputLabel="Category"
-                fullWidth
-                error={errors.category && touched.category}
-                helperText={
-                  errors.category && touched.category && errors.category
-                }
-              />
-            </Box>
-            <Box mt={2}>
-              <FieldArray name="ingredients">
-                {({ insert, remove, push }) => (
-                  <div>
-                    {values.ingredients.length > 0 &&
-                      values.ingredients.map((ingredient, index) => (
-                        <Box key={index} display="flex" alignItems="center">
-                          <Box flex={1}>
-                            <Field
-                              name={`ingredients.${index}`}
-                              as={CustomTextField}
-                              fullWidth
-                              inputLabel={`Ingredient ${index + 1}`}
-                              helperText={
-                                errors.ingredients &&
-                                errors.ingredients[index] &&
-                                touched.ingredients &&
-                                touched.ingredients[index] &&
-                                errors.ingredients[index]
-                              }
-                            />
-                          </Box>
-                          <Box ml={2}>
-                            <IconButton
-                              onClick={() => remove(index)}
-                              disabled={values.ingredients.length === 1}
-                            >
-                              <DeleteColumnOutlined />
-                            </IconButton>
-                          </Box>
-                        </Box>
-                      ))}
-                    <Box mt={2}>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        onClick={() => push("")}
-                      >
-                        Add Ingredient
-                      </Button>
-                    </Box>
-                  </div>
-                )}
-              </FieldArray>
-            </Box>
-            <Box mt={2}>
-              <FieldArray name="instructions">
-                {({ insert, remove, push }) => (
-                  <div>
-                    {values.instructions.length > 0 &&
-                      values.instructions.map((ingredient, index) => (
-                        <Box key={index} display="flex" alignItems="center">
-                          <Box flex={1}>
-                            <Field
-                              name={`instructions.${index}`}
-                              as={CustomTextField}
-                              fullWidth
-                              inputLabel={`Instuction ${index + 1}`}
-                              helperText={
-                                errors.instructions &&
-                                errors.instructions[index] &&
-                                touched.instructions &&
-                                touched.instructions[index] &&
-                                errors.instructions[index]
-                              }
-                            />
-                          </Box>
-                          <Box ml={2}>
-                            <IconButton
-                              onClick={() => remove(index)}
-                              disabled={values.instructions.length === 1}
-                            >
-                              <DeleteColumnOutlined />
-                            </IconButton>
-                          </Box>
-                        </Box>
-                      ))}
-                    <Box mt={2}>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        onClick={() => push("")}
-                      >
-                        Add Next instructions
-                      </Button>
-                    </Box>
-                  </div>
-                )}
-              </FieldArray>
-            </Box>
-            <Button type="submit">Submit</Button>
-          </>
-        </Form>
-      )}
-    </Formik>
+    <CustomCard label="Add New Recipe">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ values, errors, touched }) => (
+          <Form>
+            <Grid container spacing={2}>
+              <Grid item md={6} xl={4}>
+                <Field
+                  name="title"
+                  as={CustomTextField}
+                  inputLabel="Title"
+                  fullWidth
+                  error={errors.title && touched.title}
+                  helperText={errors.title && touched.title && errors.title}
+                />
+              </Grid>
+              <Grid item md={6} xl={4}>
+                <Field
+                  name="category"
+                  as={CustomTextField}
+                  inputLabel="Category"
+                  fullWidth
+                  error={errors.category && touched.category}
+                  helperText={
+                    errors.category && touched.category && errors.category
+                  }
+                />
+              </Grid>
+              <Grid item md={12} xl={4}>
+                <Field
+                  name="description"
+                  as={CustomTextField}
+                  inputLabel="Description"
+                  fullWidth
+                  multiline
+                  rows={4}
+                  error={errors.description && touched.description}
+                  helperText={
+                    errors.description &&
+                    touched.description &&
+                    errors.description
+                  }
+                />
+              </Grid>
+
+              <Grid item md={6} xl={4}>
+                <FieldArray name="ingredients">
+                  {({ insert, remove, push }) => (
+                    <div>
+                      {values.ingredients.length > 0 &&
+                        values.ingredients.map((ingredient, index) => (
+                          <Grid key={index} display="flex" alignItems="center">
+                            <Grid flex={1}>
+                              <Field
+                                name={`ingredients.${index}`}
+                                as={CustomTextField}
+                                fullWidth
+                                inputLabel={`Ingredient ${index + 1}`}
+                                helperText={
+                                  errors.ingredients &&
+                                  errors.ingredients[index] &&
+                                  touched.ingredients &&
+                                  touched.ingredients[index] &&
+                                  errors.ingredients[index]
+                                }
+                              />
+                            </Grid>
+                            <Grid ml={2}>
+                              <IconButton
+                                onClick={() => remove(index)}
+                                disabled={values.ingredients.length === 1}
+                              >
+                                <DeleteColumnOutlined />
+                              </IconButton>
+                            </Grid>
+                          </Grid>
+                        ))}
+                      <Grid item md={6} xl={4}>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => push("")}
+                        >
+                          Add Ingredient
+                        </Button>
+                      </Grid>
+                    </div>
+                  )}
+                </FieldArray>
+              </Grid>
+              <Grid item md={6} xl={4}>
+                <FieldArray name="instructions">
+                  {({ insert, remove, push }) => (
+                    <div>
+                      {values.instructions.length > 0 &&
+                        values.instructions.map((ingredient, index) => (
+                          <Grid key={index} display="flex" alignItems="center">
+                            <Grid flex={1}>
+                              <Field
+                                name={`instructions.${index}`}
+                                as={CustomTextField}
+                                fullWidth
+                                inputLabel={`Instuction ${index + 1}`}
+                                helperText={
+                                  errors.instructions &&
+                                  errors.instructions[index] &&
+                                  touched.instructions &&
+                                  touched.instructions[index] &&
+                                  errors.instructions[index]
+                                }
+                              />
+                            </Grid>
+                            <Grid ml={2}>
+                              <IconButton
+                                onClick={() => remove(index)}
+                                disabled={values.instructions.length === 1}
+                              >
+                                <DeleteColumnOutlined />
+                              </IconButton>
+                            </Grid>
+                          </Grid>
+                        ))}
+                      <Grid item md={6} xl={4}>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => push("")}
+                        >
+                          Add Next instructions
+                        </Button>
+                      </Grid>
+                    </div>
+                  )}
+                </FieldArray>
+              </Grid>
+              <Button type="submit">Submit</Button>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </CustomCard>
   );
 }
