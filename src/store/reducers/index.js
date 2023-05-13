6abe6fc37703220daf70d1e62b53f230218c5517
Affan-Habib <affan.habib44@gmail.com>
2023-05-13 22:36:40 +0600

    updated

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,17 +1,11 @@
 // third-party
-import { combineReducers } from 'redux';
-
-// project import
-import menu from './menu';
-import snackbar from './snackbar';
-import apiSlice from './apiSlice';
+import { combineReducers } from "redux";
+import apiSlice from "./apiSlice";
 
 // ==============================|| COMBINE REDUCERS ||============================== //
 
 const reducers = combineReducers({
-  menu,
-  snackbar,
-  api: apiSlice
+  api: apiSlice,
 });
 
 export default reducers;
