d63bd2b41efe1d943312bf0ee27a0f9221252aab
Affan-Habib <affan.habib44@gmail.com>
2023-05-20 23:00:20 +0600

    font chnage configuration updated

--- src/components/Customization.js
+++ src/components/Customization.js
@@ -4,12 +4,38 @@ import { Box, MenuItem, TextField } from "@mui/material";
 // ==============================|| HEADER CONTENT - CUSTOMIZATION ||============================== //
 
 const Customization = () => {
-  const { mode, onChangeMode } = useConfig();
+  const { mode, onChangeMode, fontFamily, onChangeFontFamily } = useConfig();
   console.log(mode);
   const handleModeChange = (event) => {
     onChangeMode(event.target.value);
   };
 
+  const handleFontChange = (event) => {
+    onChangeFontFamily(event.target.value);
+  };
+
+  const fonts = [
+    {
+      id: "inter",
+      value: `'Inter', sans-serif`,
+      label: "Inter",
+    },
+    {
+      id: "roboto",
+      value: `'Roboto', sans-serif`,
+      label: "Roboto",
+    },
+    {
+      id: "poppins",
+      value: `'Poppins', sans-serif`,
+      label: "Poppins",
+    },
+    {
+      id: "public-sans",
+      value: `'Public Sans', sans-serif`,
+      label: "Public Sans",
+    },
+  ];
   return (
     <>
       <Box sx={{ position: "absolute", top: 40, right: 40 }}>
@@ -17,8 +43,13 @@ const Customization = () => {
           <MenuItem value="light">light</MenuItem>
           <MenuItem value="dark">Dark</MenuItem>
         </TextField>
-        <TextField select defaultValue={mode} onChange={handleModeChange}>
-          <MenuItem value="Poppins">Poppins</MenuItem>
+        <TextField select defaultValue={fontFamily} onChange={handleFontChange}>
+          {fonts.map((el) => (
+            <MenuItem value={el.value} key={el.id}>
+              {el.id}
+            </MenuItem>
+          ))}
+
           <MenuItem value="Arial">Arial</MenuItem>
         </TextField>
       </Box>
